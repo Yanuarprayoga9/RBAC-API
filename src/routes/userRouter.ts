bc0a903019690f30870cd70880db7f12593c50ba
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2023-11-01 11:27:29 +0700

    add : userRouter

---
+++ src/routes/userRouter.ts
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken'
+import express from 'express'
+import { Router } from 'express'
+
+const userRouter:Router = express.Router();
+
+userRouter.post('/signIn',)
+
+
+
+
